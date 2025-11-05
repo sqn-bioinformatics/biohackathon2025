c94be9450026b26f0830b7be9c01aa85301b3789
benalex99 <benjaminforge716@gmail.com>
2025-11-05 14:02:37 +0100

    added convenience script and updated readme

--- src/run_all.py
+++ src/run_all.py
@@ -1,21 +1,8 @@
 import os
 
 from download_europepmc import main as download_data
-from embeddings import Embedder
-from update_body_from_xml import main as xml_patching, find_repo_root
-from vectordb.vectordb import VectorDB
-
-#
-# def fill_database(articles_directory):
-#     embedder = Embedder()
-#
-#     for article_name in os.listdir(articles_directory):
-#
+from update_body_from_xml import main as xml_patching
 
 if __name__ == "__main__":
     download_data()
     xml_patching()
-    #
-    # repo_root = find_repo_root(os.getcwd())
-    # database = VectorDB(os.path.join(repo_root, 'data', 'database'))
-    # fill_database(os.path.join(repo_root, 'data', 'europepmc_articles'))
