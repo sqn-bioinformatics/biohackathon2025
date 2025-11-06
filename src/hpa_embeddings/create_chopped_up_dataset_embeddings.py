cf14b86ba7213a4ffc2d9eb2139e7d1074673179
benalex99 <benjaminforge716@gmail.com>
2025-11-06 16:06:02 +0100

    refactoring and cleaning a bit, shaping document jsons

--- src/hpa_embeddings/create_chopped_up_dataset_embeddings.py
+++ src/hpa_embeddings/create_chopped_up_dataset_embeddings.py
@@ -1,16 +1,9 @@
 import time
 
-import numpy as np
-import scanpy as sc
 import sys
-import os
-import warnings
-import torch
-from pathlib import Path
 import json
-import pandas as pd
-from scipy.stats import mode
 
+import numpy as np
 import torch
 from tqdm import tqdm
 
@@ -36,53 +29,14 @@ sys.path.insert(0, "../")
 
 adata_dict, gene_col = load_datasets()
 
-print("print(adata_dict.keys())", adata_dict.keys())
-print('adata_dict["251020_mann2017.h5ad"]', adata_dict["251020_mann2017.h5ad"])
-print('adata.obs["cell_type"]', adata_dict["251020_mann2017.h5ad"].obs)
-# print('adata.obs["cell_type"]', adata_dict["251020_mann2017.h5ad"].obs["cell_type"])
-print('adata_dict["251020_mann2017.h5ad"].var', adata_dict["251020_mann2017.h5ad"].var)
-print('adata_dict["251020_mann2017.h5ad"].uns', adata_dict["251020_mann2017.h5ad"].uns.keys())
-
-
-
-adata = adata_dict["251020_mann2017.h5ad"]
-
-# count_matrix = (
-#     adata.layers["counts"]
-#     if isinstance(adata.layers["counts"], np.ndarray)
-#     else adata.layers["counts"].A
-# )
-#
-# print("count_matrix", count_matrix)
-# print("count_matrix", count_matrix.shape)
-
-
-# # Ensure count_matrix is a proper numpy array with copy to avoid issues
-# count_matrix = np.array(count_matrix, dtype=np.float32)
-#
-# # gene vocabulary ids
-# if gene_ids is None:
-#     gene_ids = np.array(adata.var["id_in_vocab"])
-#     assert np.all(gene_ids >= 0)
-#
-# # Ensure gene_ids is a proper numpy array
-# gene_ids = np.array(gene_ids, dtype=np.int64)
-
-
-# print('adata', adata.layers.keys())
-print("adata.obs_names", len(adata.obs_names), adata.obs_names)
-print("adata.var_names", len(adata.var_names), adata.var_names)
-
-print("adata", adata)
-
-rows = []
+documents = []
 lineages = dict()
 celltypes = dict()
 for dataset_name, adata in adata_dict.items():
     num_genes = adata.X.shape[0] * adata.X.shape[1]
     num_genes_kept = 0
     for row in tqdm(adata):
-        row.write('my_results.h5ad', compression="gzip")
+        # row.write('my_results.h5ad', compression="gzip")
         # print("row", row)
         # print("row.obs", list(row.obs.keys()))
         # print("row.obs", row.obs.values)
@@ -91,31 +45,43 @@ for dataset_name, adata in adata_dict.items():
         # print(row.var)
         # print("Genes", list(row.var.keys()))
         # print("Genes:", len(row.var['index'].values), row.var['index'].values)
-        celltypes[row.obs['cell_type'][0]] = 0
+
+        # Obtain all celltypes
+        celltype = row.obs['cell_type'].values[0]
+        celltypes[celltype] = 0
+
         row_metadata = dict(zip(list(row.obs.keys()), row.obs.values[0])) # celltype and lineage
-        row_metadata["type"] = "data"
-        row_metadata["dataset name"] = dataset_name
+        row_metadata["type"] = "dataset"
+        row_metadata["dataset_name"] = dataset_name
+        row_metadata["dataset_description"] = \
+            "This is a row from a dataset about Celltypes, their correpsonding lineages " \
+            "and their corresponding gene counts. "
+        # TODO: Use Lornas mappings to convert the celltype to synonims and ontologies and add these to the meta data
         # row_metadata["meshterms"] =
         # row_metadata["ontology_tree"] =
-        row_genes = dict(zip(row.var['index'].values, row.X[0].data))
 
+        row_genes = dict(zip(row.var['index'].values, np.array(row.X[0].data).astype(np.int64).tolist()))
         for key in list(row_genes.keys()):
             if row_genes[key] == 0:
                 del row_genes[key]
-        # print("num genes left", len(list(row_genes.keys())))
         num_genes_kept += len(list(row_genes.keys()))
-        row = str(row_metadata) + " " + str(row_genes)
-        # print("result", row)
-        # print("Genes", row.var["index"][:, 0])
 
-        rows.append(row)
+        document = {
+            "metadata": {
+                "pmcid": None,
+                "author": None,
+                "License": None,
+                "source_url": None,
+                "dataset_meta": json.dumps(row_metadata)},
+            "body": json.dumps(row_genes)}
+        documents.append(document)
+
     print("kept", num_genes_kept, "out of", num_genes, "so", (num_genes_kept / num_genes) * 100, "%")
 
 print("celltypes", len(list(celltypes.keys())), list(celltypes.keys()))
 
-# for row in adata.to_df():
-#     print("row")
-#     print(row)
+print("example document")
+print(documents[0])
 
 time.sleep(10)
 
